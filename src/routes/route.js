89cf182ac492d40504b12252417d06cf318e82b5
dey-animesh98 <102967934+dey-animesh98@users.noreply.github.com>
2022-04-08 01:42:04 +0530

    7th April

--- src/routes/route.js
+++ src/routes/route.js
@@ -20,5 +20,60 @@ router.get('/test-me', function (req, res) {
 
 
 
+
+router.get('/movies', function(req,res){
+    const movieList = ["Titanic", "Conjuring", "Superman", "Avengers", "Harry Potter", "3 Iditots"]
+    res.send(movieList)
+
+})
+
+
+router.get('/movies/:indexNumber', function(req,res){
+    const movieList1 = ["Titanic", "Conjuring", "Superman", "Avengers", "Harry Potter", "3 Iditots"]
+    const movieId = req.params.indexNumber
+    for ( let i =0 ; i < movieList1.length; i++){
+        if (movieId < movieList1.length){
+            return res.send(movieList1[movieId])
+        }else if(movieId > movieList1.length){
+            return res.send("Invalid user input")
+        }      
+    }  
+})
+
+
+router.get('/films', function(req,res){
+    const filmList =[
+                         {"id" :1, "name" : "Titanic"},
+                         {"id" :2, "name" : "Conjuring"},
+                         {"id" :3, "name" : "Superman"},
+                         {"id" :4, "name" : "Avengers"},
+                         {"id" :5, "name" : "Harry Potter"},
+                    ]
+
+    res.send(filmList)
+})
+
+router.get('/films/:filmId', function(req,res){
+    const filmList =[
+                         {"id" :1, "name" : "Titanic"},
+                         {"id" :2, "name" : "Conjuring"},
+                         {"id" :3, "name" : "Superman"},
+                         {"id" :4, "name" : "Avengers"},
+                         {"id" :5, "name" : "Harry Potter"},
+                    ]
+   
+    const getFilmId = req.params.filmId
+    for ( let i =0 ; i < filmList.length; i++){
+        const myFilm = filmList[i]
+        if( filmList.id === getFilmId){
+            return res.send(filmList[myFilm])
+        }
+     }  
+
+    
+})
+
+
+
 module.exports = router;
 // adding this comment for no reason
