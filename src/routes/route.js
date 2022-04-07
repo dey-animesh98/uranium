e45f5b3d7eedc8c87654d510c156ec2f13bf5154
dey-animesh98 <102967934+dey-animesh98@users.noreply.github.com>
2022-04-07 22:21:04 +0530

    update

--- src/routes/route.js
+++ src/routes/route.js
@@ -7,13 +7,29 @@ router.get('/test-me', function (req, res) {
     console.log('------------------')
     console.log(req)
     console.log('------------------')
-    console.log('These are the request query parameters: ', req.query)
-    res.send('My first ever api!')
+    console.log('These are the request query parameters: ',req.query)
+    res.send('My Api')
 });
 
 
-router.get('/candidates', function (req,res){
-    res.send('[a],[b],')
+router.get('/all-candidates', function (req,res){
+    
+    const listOFCandidates =["Rahul","Jay","Ram", "Raju", "Avi","Vimal", "Azad", "Kanai","Raja", "Rahim"]
+    
+    res.send(listOFCandidates)
+})
+
+router.get('/candidates', function(req, res){
+   
+    const listOFCandidates =["Rahul","Jay","Ram", "Raju", "Avi","Vimal", "Azad", "Kanai","Raja", "Rahim"]
+    for (let i=0; i<listOFCandidates.length; i++ ) {
+        let finalCandidate = listOFCandidates[i] 
+    }
+//    console.log(req.query.key)
+    res.send ({
+        finalCandidate : req.query.key}
+    )
+
 })
 
 module.exports = router;
