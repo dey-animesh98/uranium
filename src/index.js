5795b819e7066cbc6ab8678c06cc8ff1a6e8540e
Animesh Dey <animesh.dey986@gmail.com>
2022-04-22 01:41:22 +0530

    assignment complete

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://users-open-to-all:hiPassword123@cluster0.uh35t.mongodb.net/skDB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://animesh-dey98:9I9JRLwql3bINqUX@cluster0.vhmqo.mongodb.net/animesh-deyDB", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
